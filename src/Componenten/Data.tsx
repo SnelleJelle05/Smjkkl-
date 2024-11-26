62cc617cdd15e89464dd2f21e27506694b42eb85
strafeeeee <178388@student.aventus.nl>
2024-11-26 11:46:16 +0100

    Version 1.2

--- src/Componenten/Data.tsx
+++ src/Componenten/Data.tsx
@@ -11,7 +11,13 @@ const Data: React.FC<BasicComponentProps> = ({ Title }) => {
             <div className={"header"}>
                 <h1 className={"h1Data"}>{Title}</h1>
             </div>
-
+            <div className="divData">
+                <div className="columnHeader">Project naam</div>
+                <div className="columnHeader">Uren</div>
+                <div className="columnHeader">Status</div>
+                <div className="columnHeader">Gestart op</div>
+            </div>
+            <hr className={'line'}/>
         </div>
     );
 };
